04631196cea3fc05e0cc91223d00ec90d44f81ae
Luiso <chineamujicaluisenrique@gmail.com>
2022-07-06 00:39:07 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import styles from "./App.module.css";
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import {  Link } from "react-router-dom";
 import { MovieDetails } from "./pages/movieDetails";
 import { LandingPage } from "./pages/landingPage";
@@ -17,6 +17,7 @@ export function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/movie/:movieId" element={<MovieDetails />} />
+        <Route path="*" element={<Navigate replace to= "/" />} />     {/* esta ruta es pa si el usuario pone un ruta erronea en la url lo redirccione al home */}
       </Routes>
     </BrowserRouter>
   );
